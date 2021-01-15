b4148cbbca81c79cfdfc050bca29fcde3a2cfdf3
Oliver Erdmann <oerdmann@uwaterloo.ca>
2021-01-14 20:31:09 -0800

    add start

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.get('/', (req,res)=> {
 });
 
 
+const port = process.env.PORT || 3000;
 
-
-app.listen(3000, ()=> {
-    console.log('Serving on port 3000');
+app.listen(port, ()=> {
+    console.log(`Serving on port ${port}`);
 });
